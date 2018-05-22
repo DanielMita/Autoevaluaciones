180094a177904756da3290edf0dbe4f88f7dfd4b
DanielMita <daniel_mita@live.com>
2018-05-22 22:30:53 +0200

    f

---
+++ Autoevaluaciones/src/Autoevaluacion4/Principal.java
@@ -0,0 +1,31 @@
+package Autoevaluacion4;
+
+class Usuario {
+
+    //Atributos
+    public String nif;
+    public String nombre;
+    public String apellidos;
+    public String correo;
+    public String domicilio;
+    public String fechaNacimiento;
+    public String fechaAlta;
+    public String claveAcceso;
+    public String rol;
+
+} //class
+
+public class Principal {
+
+    final static int MAX_USUARIOS = 45;
+
+    // Almacén de datos resuelto con arrays
+    public static Usuario[] datosUsuarios = new Usuario[MAX_USUARIOS];
+    public static void main(String[] args) {
+
+//...
+    }
+
+ //...
+
+} //class
